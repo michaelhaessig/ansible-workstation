88e770149572d47557aac9a2cbf71c473e70b9cf
miki4you <michael.haessig@gmail.com>
2016-04-17 20:05:26 +0200

    initial commit

---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y git python-pip python-paramiko #python-setuptools
+sudo pip install setuptools
+sudo pip install paramiko PyYAML Jinja2 httplib2 six markupsafe
+
+
+git clone git://github.com/ansible/ansible.git --recursive
+
+source ./ansible/hacking/env-setup
+
+git clone https://github.com/miki4you/ansible-workstation.git
+
